4b0a7b994457a87d10c1508597e7ec652c9e7277
briannovillo <brian@briannovillo.com.ar>
2019-10-18 09:20:13 -0300

    Improve config get

--- src/controllers/product/MeliDataProvider.ts
+++ src/controllers/product/MeliDataProvider.ts
@@ -2,16 +2,16 @@ import fetch from 'node-fetch';
 const config = require('config');
 
 export const searchProductsByTitle = async (query: string) => {
-    return await fetch(`${config.get("HOSTS.MELI_API")}${config.get("ENDPOINTS.MELI_API_SEARCH")}?q=${query}`)
+    return await fetch(`${config.get("hosts.meliApi")}${config.get("endpoints.meliApiSearch")}?q=${query}`)
                     .then(res => res.json())
                     .then(res => res.results);
 };
 
 export const getProductsById = async (id: string) => {
-    const productPromise = fetch(`${config.get("HOSTS.MELI_API")}${config.get("ENDPOINTS.MELI_API_ITEMS")}/${id}`)
+    const productPromise = fetch(`${config.get("hosts.meliApi")}${config.get("endpoints.meliApiItems")}/${id}`)
                                 .then(res => res.json());
 
-    const productDescriptionPromise = fetch(`${config.get("HOSTS.MELI_API")}${config.get("ENDPOINTS.MELI_API_ITEMS")}/${id}/description`)
+    const productDescriptionPromise = fetch(`${config.get("hosts.meliApi")}${config.get("endpoints.meliApiItems")}/${id}/description`)
                                             .then(res => res.json());
 
     const promises = await Promise.all([productPromise, productDescriptionPromise]);
